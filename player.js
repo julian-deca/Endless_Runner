e4d4043f7caf5a24dbc5c5805095dbe13ab323f9
julian <juliandecastelli@gmail.com>
2022-10-18 20:54:51 -0300

    add assets

---
+++ player.js
@@ -0,0 +1,11 @@
+class Player {
+  constructor(game) {
+    this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = 0;
+    this.y = 100;
+  }
+  update() {}
+  draw(ctx) {}
+}
--- script.js
+++ script.js
@@ -3,4 +3,13 @@ window.addEventListener("load", () => {
   const ctx = canvas.getContext("2d");
   canvas.width = 500;
   canvas.height = 500;
+
+  class Game {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+    update() {}
+    draw() {}
+  }
 });
