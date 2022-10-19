0c5877ff3c934f5e49a899cd290f724c34c805a7
julian <juliandecastelli@gmail.com>
2022-10-19 12:21:40 -0300

    add update and draw method

--- player.js
+++ player.js
@@ -1,11 +1,29 @@
-class Player {
+export default class Player {
   constructor(game) {
     this.game = game;
     this.width = 100;
     this.height = 91.3;
-    this.x = 0;
-    this.y = 100;
+    this.x = 200;
+    this.y = this.game.height - this.height;
+    this.image = player;
+    this.frameX = 0;
+    this.frameY = 0;
   }
-  update() {}
-  draw(ctx) {}
+  update() {
+    //this.x++;
+  }
+  draw(context) {
+    //context.fillRect(this.x, this.y, this.width, this.height);
+    context.drawImage(
+      this.image,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
 }
