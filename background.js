497b03eae61ba9b904e4b5f6679153137c8bd226
julian <juliandecastelli@gmail.com>
2022-10-20 18:02:15 -0300

    add layer class

---
+++ background.js
@@ -0,0 +1,24 @@
+class Layer {
+  constructor(game, width, height, speedModifier, image) {
+    this.game = game;
+    this.width = width;
+    this.height = height;
+    this.speedModifier = speedModifier;
+    this.image = image;
+    this.x = 0;
+    this.y = 0;
+  }
+  update() {
+    if (this.x < -this.width) this.x = 0;
+    else this.x -= this.game.speed * this.speedModifier;
+  }
+  draw() {
+    constext.drawImage(
+      this.image,
+      this.x,
+      this.y,
+      this.draw.width,
+      this.height
+    );
+  }
+}
