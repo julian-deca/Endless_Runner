06baeed85c33f90d33b64648aad05331d6b9db2a
julian <juliandecastelli@gmail.com>
2022-10-23 11:04:55 -0300

    add groundEnemy class

--- script.js
+++ script.js
@@ -46,6 +46,8 @@ window.addEventListener("load", () => {
       });
     }
     addEnemy() {
+      if (this.speed > 0 && Math.random() < 0.5)
+        this.enemies.push(new GroundEnemy(this));
       this.enemies.push(new FlyfingEnemy(this));
       console.log(this.enemies);
     }
