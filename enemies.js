3f1e5a38b5d02b8052d6b52ec7130f02e1687d57
julian <juliandecastelli@gmail.com>
2022-10-23 11:51:36 -0300

    add climbingEnemy class

--- enemies.js
+++ enemies.js
@@ -67,4 +67,31 @@ export class GroundEnemy extends Enemy {
     this.maxFrame = 1;
   }
 }
-export class ClimbingEnemy extends Enemy {}
+export class ClimbingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 120;
+    this.height = 144;
+
+    this.x = this.game.width;
+    this.y = Math.random() * this.game.height * 0.5;
+    this.image = enemy_spider_big;
+    this.speedX = 0;
+    this.speedY = Math.random() > 0.5 ? 1 : -1;
+    this.maxFrame = 5;
+  }
+  update(deltatime) {
+    super.update(deltatime);
+    if (this.y > this.game.height - this.height - this.game.groundMargin)
+      this.speedY *= -1;
+    else if (this.y < -this.height) this.markedForDeletion = true;
+  }
+  draw(context) {
+    super.draw(context);
+    context.beginPath();
+    context.moveTo(this.x + this.width * 0.5, 0);
+    context.lineTo(this.x + this.width * 0.5, this.y + 60);
+    context.stroke();
+  }
+}
--- script.js
+++ script.js
@@ -48,6 +48,7 @@ window.addEventListener("load", () => {
     addEnemy() {
       if (this.speed > 0 && Math.random() < 0.5)
         this.enemies.push(new GroundEnemy(this));
+      else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
       this.enemies.push(new FlyfingEnemy(this));
       console.log(this.enemies);
     }
